242cddc5d6c3d1419181958d28b2ee54899bc597
vitaliytv <vitaliytv@nitralabs.com>
2022-08-16 17:48:38 +0300

    1.1.3

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# jwt decode
+# @nitra/bunyan
 
 [![Mega-Linter](https://github.com/nitra/bunyan/workflows/Mega-Linter/badge.svg?branch=main)](https://github.com/nitra/bunyan/actions?query=workflow%3AMega-Linter+branch%3Amain)
 
-## Decode token
+## Cloud Run Job
 
 ```JavaScript
 
 import log from '@nitra/bunyan'
 
+log.info(123456)
+```
+
+## Cloud Run with trace
+
+```JavaScript
+
+import getLogger from '@nitra/bunyan/trace'
+
+const log = getLogger(req)
 log.info(123456)
 ```
--- src/trace.js
+++ src/trace.js
@@ -2,20 +2,15 @@ import bunyan from 'bunyan'
 import { loggerOption, traceKey } from './options.js'
 import gcpMetadata from 'gcp-metadata'
 
-const projectId = await gcpMetadata.project('project-id')
-// TODO:
-console.log('Detected project id:', projectId)
+let projectId
+if (traceKey) {
+  projectId = await gcpMetadata.project('project-id')
+}
 
 export default req => {
   if (traceKey) {
-    // TODO:
-    console.log('Dheders:', req.headers)
-
-    const traceHeader = req && req.headers ? req.headers['x-cloud-trace-context'] || '' : ''
+    const traceHeader = req?.headers ? req.headers['x-cloud-trace-context'] || '' : ''
     const traceId = traceHeader ? traceHeader.split('/')[0] : ''
-
-    // TODO:
-    console.log('traceId:', traceId)
 
     if (traceId) {
       loggerOption[traceKey] = `projects/${projectId}/traces/${traceId}`
