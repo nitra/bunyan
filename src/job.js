4bcdcaa8fc992a19211b9d35f585011f65af8edd
elenatv <elena@nitralabs.com>
2023-09-05 20:12:08 +0300

    2.0.1

--- src/job.js
+++ src/job.js
@@ -2,18 +2,15 @@ import bunyan from 'bunyan'
 import { loggerOption, traceKey } from './options.js'
 import { getProjectId } from '@nitra/gcp-metadata'
 
-const log = bunyan.createLogger(loggerOption)
+let log = bunyan.createLogger(loggerOption)
 
-let logE
 if (traceKey) {
   // const projectId = await getProjectId()
 
   // Для Binary - Top-level await is currently not supported with the "cjs" output format
   getProjectId().then(projectId => {
-    logE = log.child({ [traceKey]: `projects/${projectId}/traces/${process.env.CLOUD_RUN_EXECUTION}` }, true)
+    log = log.child({ [traceKey]: `projects/${projectId}/traces/${process.env.CLOUD_RUN_EXECUTION}` }, true)
   })
-} else {
-  logE = log
 }
 
-export default logE
+export default log
