4052879c4e4d42080080c3268345c1bc2592cc7b
vitaliytv <v@nitra.ai>
2024-08-23 13:24:08 +0300

    https://github.com/googleapis/nodejs-logging/issues/1520

--- npm/src/options.js
+++ npm/src/options.js
@@ -1,5 +1,5 @@
 import { create, serializers } from 'bunyan-debug-stream'
-import { LoggingBunyan, LOGGING_TRACE_KEY } from '@google-cloud/logging-bunyan'
+// import { LoggingBunyan, LOGGING_TRACE_KEY } from '@google-cloud/logging-bunyan'
 
 export let loggerOption
 export let traceKey
@@ -7,25 +7,38 @@ export let traceKey
 const name = process.env.CLOUD_RUN_JOB || process.env.K_SERVICE
 
 if (name) {
-  traceKey = LOGGING_TRACE_KEY
-  const level = process.env.LOG_LEVEL || 'info'
-
-  // Creates a Bunyan Cloud Logging client
-  const loggingBunyan = new LoggingBunyan()
-
-  // Create a Bunyan logger that streams to Cloud Logging
-  // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
   loggerOption = {
-    // The JSON payload of the log as it appears in Cloud Logging
-    // will contain "name": "catalina-job-sync-from-b2b"
-    name,
+    name: 'local',
     streams: [
-      // Log to the console at 'info' and above
-      { stream: process.stdout, level },
-      // And log to Cloud Logging, logging at 'info' and above
-      loggingBunyan.stream(level)
-    ]
+      {
+        level: 'debug',
+        type: 'raw',
+        stream: create({
+          forceColor: true
+        })
+      }
+    ],
+    serializers
   }
+  // traceKey = LOGGING_TRACE_KEY
+  // const level = process.env.LOG_LEVEL || 'info'
+
+  // // Creates a Bunyan Cloud Logging client
+  // const loggingBunyan = new LoggingBunyan()
+
+  // // Create a Bunyan logger that streams to Cloud Logging
+  // // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
+  // loggerOption = {
+  //   // The JSON payload of the log as it appears in Cloud Logging
+  //   // will contain "name": "catalina-job-sync-from-b2b"
+  //   name,
+  //   streams: [
+  //     // Log to the console at 'info' and above
+  //     { stream: process.stdout, level },
+  //     // And log to Cloud Logging, logging at 'info' and above
+  //     loggingBunyan.stream(level)
+  //   ]
+  // }
 } else {
   loggerOption = {
     name: 'local',
