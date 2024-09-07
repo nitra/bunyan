1e2b054c06e655a124cd02b5d5bf655a95341ac8
vitaliytv <v@nitra.ai>
2024-09-07 16:41:10 +0300

    restore google cloud logging

--- npm/src/options.js
+++ npm/src/options.js
@@ -1,5 +1,5 @@
+import { LOGGING_TRACE_KEY, LoggingBunyan } from '@google-cloud/logging-bunyan'
 import { create, serializers } from 'bunyan-debug-stream'
-// import { LoggingBunyan, LOGGING_TRACE_KEY } from '@google-cloud/logging-bunyan'
 
 export let loggerOption
 export let traceKey
@@ -20,25 +20,25 @@ if (name) {
     ],
     serializers
   }
-  // traceKey = LOGGING_TRACE_KEY
-  // const level = process.env.LOG_LEVEL || 'info'
+  traceKey = LOGGING_TRACE_KEY
+  const level = process.env.LOG_LEVEL || 'info'
 
-  // // Creates a Bunyan Cloud Logging client
-  // const loggingBunyan = new LoggingBunyan()
+  // Creates a Bunyan Cloud Logging client
+  const loggingBunyan = new LoggingBunyan()
 
-  // // Create a Bunyan logger that streams to Cloud Logging
-  // // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
-  // loggerOption = {
-  //   // The JSON payload of the log as it appears in Cloud Logging
-  //   // will contain "name": "catalina-job-sync-from-b2b"
-  //   name,
-  //   streams: [
-  //     // Log to the console at 'info' and above
-  //     { stream: process.stdout, level },
-  //     // And log to Cloud Logging, logging at 'info' and above
-  //     loggingBunyan.stream(level)
-  //   ]
-  // }
+  // Create a Bunyan logger that streams to Cloud Logging
+  // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
+  loggerOption = {
+    // The JSON payload of the log as it appears in Cloud Logging
+    // will contain "name": "catalina-job-sync-from-b2b"
+    name,
+    streams: [
+      // Log to the console at 'info' and above
+      { stream: process.stdout, level },
+      // And log to Cloud Logging, logging at 'info' and above
+      loggingBunyan.stream(level)
+    ]
+  }
 } else {
   loggerOption = {
     name: 'local',
